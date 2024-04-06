d9574ccae40f5932b07e5c7c4795b9f2f6587cfa
Shubham Kumar <40725950+Ershubhambhagat@users.noreply.github.com>
2024-04-06 00:47:09 -0400

    Update AutomaticFlowControlSystem.ino

--- AutomaticFlowControlSystem.ino
+++ AutomaticFlowControlSystem.ino
@@ -1,9 +1,6 @@
 const int pingPin = 2; // Trigger Pin of Ultrasonic Sensor
 const int echoPin = 5;// Echo Pin of Ultrasonic Sensor
 const int relayPin = 8; //
-
-
-
 int FirstWaterLableCheck;
 //const long interval = 300000;      // 5 minutes in milliseconds
 const long WaitForIncreaseWater =5000;  //5 sec  
@@ -12,14 +9,10 @@ const long DeepSleep = 300;
 const int OnMotor15 = 15;   // 15 FirstWaterLableCheck
 const int OffMotor10 = 10;  // 80 FirstWaterLableCheck
 const int SensorNotWorking35 = 35;
-
- 
 void setup() {
   pinMode(relayPin, OUTPUT);
-  
   Serial.begin(9600); // Starting Serial Terminal
 }
-
 void loop() {
    long duration, WaterLevel1;
    pinMode(pingPin, OUTPUT);
@@ -46,14 +39,13 @@ void loop() {
   }
  else if (WaterLevel1 >=SensorNotWorking35) ///36>=35
   {
-    
     Serial.println("Sensor Not Working");
     Serial.println();
+     digitalWrite(relayPin, LOW);
     Serial.print("Going To Deep Sleep");
     Serial.println();
     //delay(DeepSleep);
   }
-
   // Pump On Logic 
   else if ( OnMotor15 <= WaterLevel1 ) //15<24
   {
@@ -61,15 +53,12 @@ void loop() {
     digitalWrite(relayPin, HIGH);
     Serial.print("Relay On" );
     Serial.println();
-
     Serial.print("Low Water Level Going to Start Motor" );
     Serial.println();
-   
     Serial.print("Checkig ......" );
-
     Serial.println();
     delay(WaitForIncreaseWater);//WaitForIncreaseWater
-    long WaterLevel1;
+    int WaterLevel1;
    pinMode(pingPin, OUTPUT);
    digitalWrite(pingPin, LOW);
    delayMicroseconds(2);
@@ -80,39 +69,27 @@ void loop() {
    duration = pulseIn(echoPin, HIGH);
    WaterCheckAfterMin = microsecondsToInches(duration);
    Serial.print("New Water Lavel:- ");
-
    Serial.print(WaterCheckAfterMin);
-  
     Serial.println();
    //delay(100);
-   if (WaterCheckAfterMin > WaterLevel1)
-   
+   if (WaterCheckAfterMin >= WaterLevel1)//21>=22
    {
     Serial.print("Water not Increase after Min ");
     Serial.println();
-
+     digitalWrite(relayPin, LOW);
     Serial.print("Motor Off Please ");
     Serial.println();
-
-    Serial.print("Again Wait For Increase Water ");
     Serial.println();
-
    Serial.print(WaterCheckAfterMin);
-
-
     delay(WaitForIncreaseWater);//WaitForIncreaseWater
-
-
-    
    }
-   else if (WaterCheckAfterMin < WaterLevel1) //18<28 , 13<30
+   else if (WaterLevel1>WaterCheckAfterMin) // 18 < 21< 22 18<28 , 13<30
       {
     Serial.print("Water Increase after Min ");
     Serial.println();
     long duration, WaterCheckAfterMin;//18>10
-    while (WaterCheckAfterMin >= OffMotor10)//23>10 ,13>10,17>10
+    while (WaterCheckAfterMin >= OffMotor10)//
     {
-
       pinMode(pingPin, OUTPUT);
       delayMicroseconds(2);
       digitalWrite(pingPin, HIGH);
@@ -130,23 +107,17 @@ void loop() {
     }
       }   
   }
-  
   else
   {
     Serial.print("Else ..........");
     Serial.println();
-
     Serial.print("Tank is Full or Water Not Increase:= ");
     Serial.println();
-
     digitalWrite(relayPin, LOW);
     Serial.print(WaterLevel1);
-    
     //delay(intervaloraginal);
   }
 }
-
 long microsecondsToInches(long microseconds) {
    return microseconds / 74 / 2;
 }
-
